4b0cebb218e39c7a95ac0b4e08d47123d70773ba
Luís Vinicius Capelletto <thecapellett@gmail.com>
2022-01-24 13:19:20 -0300

    updates fix

--- Code/Exercicios-Lista/01_lista_4_iguais.hs
+++ Code/Exercicios-Lista/01_lista_4_iguais.hs
@@ -1,5 +1,5 @@
 iguais_4 :: [Int] -> Bool
-iguais_4 list 
+iguais_4 list
   | length list == 4 = iguais list
   | otherwise = False
 
@@ -7,15 +7,15 @@ iguais_4 list
 iguais :: [Int] -> Bool
 iguais [] = True
 iguais [_] = True
-iguais (x:xs) = x == (head xs) && iguais xs 
+iguais (x:xs) = x == (head xs) && iguais xs
 
 
-{- 
+{-
   1. Verificar se em uma lista de 4 elementos, todos sao iguais, retorna bool
-  - Verifique se possui 4 elementos 
+  - Verifique se possui 4 elementos
   - Verifique se todos elementos sao iguais
 
-  ] -> False
+  [] -> False
   [1] -> False
   [0,0,0,0] -> True
 
--- README.md
+++ README.md
@@ -3,7 +3,9 @@
   <img src=".github/Haskell_logo.png" width="200">
 </div>
 
-Repositório de códigos de fundamentos da linguagem Haskell
+## Repositório de códigos de fundamentos da linguagem Haskell
 
-- Exercícios de códigos feitos em Haskell (Hugs98)
+Contém:
+
+- Exercícios de códigos feitos em Haskell (incluindo Hugs98)
 - Materiais de referência teóricos
